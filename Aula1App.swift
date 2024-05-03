020d74a34df91566650d79c59c78788a7a8ebdc0
Juliacgrando <114629830+juliacgrando@users.noreply.github.com>
2024-05-03 08:32:07 -0300

    Add files via upload
    Aula 1 hackatruck SwiftUI

---
+++ Aula1App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula1App.swift
+//  Aula1
+//
+//  Created by Turma01-23 on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,60 @@
+
+import SwiftUI
+
+/*struct ContentView: View {
+    var body: some View {
+ 
+        HStack{
+            Rectangle()
+                .frame(width:100,height: 100)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .padding()
+            Spacer()
+            Rectangle()
+                .frame(width:100, height: 100)
+                .foregroundColor(.red)
+                .padding()
+        }
+ 
+            Spacer()
+ 
+        HStack{
+            Rectangle()
+                .frame(width:100,height: 100)
+                .foregroundColor(.purple)
+                .padding()
+            Spacer()
+            Rectangle()
+                .frame(width:100,height: 100)
+                .foregroundColor(.green)
+                .padding()
+            }
+        }
+    }
+
+struct ContentView: View{
+    var body: some View{
+        VStack{
+            HStack{
+                Image("gatomeme")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .padding()
+                VStack(spacing:10){
+                    Text("ALOUUUUUUUU")
+                        .foregroundStyle(.red)
+                    Text(" Galera de peao")
+                        .foregroundStyle(.blue)
+                    Text("quem ta feliz bate forte com a máo")
+                        .foregroundStyle(.green)
+                }
+                .padding()
+                }
+            }
+        }
+    }*/
+
+    #Preview {
+    ContentView()
+}
